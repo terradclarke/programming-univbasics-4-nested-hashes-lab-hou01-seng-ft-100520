675d9d29a08419ecaab09304929d82b6a4283ebf
Terranie Sims <terranie67@gmail.com>
2020-09-11 00:01:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,11 +15,12 @@ def hopper
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:grace_hopper]
 end
 
+
+
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
 	programmer_hash = 
  		{
@@ -36,6 +37,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+  programmer_hash[:alan_kay][:known_for]
 
 end
 
@@ -55,16 +57,11 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+  programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#   :languages => ["LISP", "C"]
-# }
-# return the entire updated hash
 
 	programmer_hash = 
  		{
@@ -81,6 +78,8 @@ def adding_matz
         :languages => ["C"]
       }
     }
+  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+  programmer_hash
 
 end
 
