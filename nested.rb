13240458fd02fe10167ee611829442f090377d04
Terranie Sims <terranie67@gmail.com>
2020-09-11 00:02:09 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,8 +84,6 @@ def adding_matz
 end
 
 def changing_alan
-  # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-  # return the entire updated hash
 
 	programmer_hash = 
  		{
@@ -102,6 +100,9 @@ def changing_alan
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+    programmer_hash
 
 end
 
